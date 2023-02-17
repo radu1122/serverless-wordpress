0b982ce80861640ee76f01640438faf4a0f968b6
Radu Dumitrescu <r.dumitrescu@reply.com>
2023-02-17 11:23:55 +0200

    small fix

--- server/config/helper.js
+++ server/config/helper.js
@@ -1,1 +1,13 @@
-export const MONGO_DB_URI = "mongodb+srv://genezio:genezio@cluster0.c6qmwnq.mongodb.net/?retryWrites=true&w=majority"
+export const MONGO_DB_URI =
+  "mongodb+srv://genezio:genezio@cluster0.c6qmwnq.mongodb.net/?retryWrites=true&w=majority";
+
+import { ActiveSession } from "./../models/activeSession";
+
+export async function reqAuth(token) {
+  const session = await ActiveSession.find({ token: token });
+  if (session.length == 1) {
+    return { success: true };
+  } else {
+    return { success: false, msg: "User is not logged on" };
+  }
+}
--- server/controllers/options.js
+++ server/controllers/options.js
@@ -1,7 +1,37 @@
+import { mongoose } from "mongoose"
+import { reqAuth, MONGO_DB_URI } from "./helper"
+import { wp_options } from "../models/wp_options"
+
+/**
+ * req: {token: "token", body: {}}
+ * res: {success: true, msg: "success"} / { success: false, msg: "User is not logged on" }
+ */
 
 
 export class OptionsController {
-  
+  constructor() {
+    this.#connect();
+  }
+
+  /**
+   * Private method used to connect to the DB.
+   */
+  #connect() {
+    mongoose.connect(MONGO_DB_URI);
+  }
+
+  // get all options from the database
+  async getOptions(req) {
+    const authObject = await reqAuth(token);
+    if (!authObject.success) {
+      return authObject;
+    }
+
+    return await wp_options.find();
+  }
+
+
+
 
 
 }
